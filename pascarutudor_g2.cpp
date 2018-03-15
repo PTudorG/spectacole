ea2ebfcee21f1d7b70c45df32eec08a3a5324241
PTudorG <35453013+PTudorG@users.noreply.github.com>
2018-03-15 11:36:00 +0200

    Update pascarutudor_g2.cpp

--- pascarutudor_g2.cpp
+++ pascarutudor_g2.cpp
@@ -2,34 +2,36 @@
 #include<iostream>
 using namespace std;
 float M[100][2];
-int n,k,g,aux;
+int n,k,g,aux,nr[100];
 
 int read_data()
 {fstream f;
 f.open("inputs.dat",ios::in);
 f>>n;
 f>>k;
-for (int i=1;i<=n;i++)
-{for (int j=1;j<=2;j++)
+for (int i=0;i<n;i++)
+{nr[i]=i+1;
+for (int j=0;j<=1;j++)
 f>>M[i][j];
-M[i][2]=M[i][2]+(k/100.0);
-if(M[i][2]-int(M[i][2])>=0.6)
-{M[i][2]=M[i][2]+1;
-M[i][2]=M[i][2]-0.6;}}}
+M[i][1]=M[i][1]+(k/100.0);
+if(M[i][1]-int(M[i][1])>=0.6)
+{M[i][1]=M[i][1]+1;
+M[i][1]=M[i][1]-0.6;}}}
 
 int sort_data()
 {do{g=0;
-   for(int i=1;i<=n-1;i++)
-       if(M[i][2]>M[i+1][2])
-         {aux=M[i][2];
-          M[i][2]=M[i+1][2];
-          M[i+1][2]=aux;g=1;}}
+   for(int i=0;i<n-1;i++)
+       if(M[nr[i]][1]>M[nr[i+1]][1])
+         {aux=nr[i];
+          nr[i]=nr[i+1];
+          nr[i+1]=aux;g=1;}}
     while(g!=0);}
 
 float spectacole()
-{for (int ultimul=0,i=1;i<n;i++)
-if (M[i][1]>=M[ultimul][2])
-{cout<<i+1<<" ";
+{int ultimul=0;
+for (int i=1;i<n;i++)
+if (M[nr[i]][0]>=M[nr[ultimul]][1])
+{cout<<nr[i]+1<<" ";
 ultimul=i;}}
 
 
